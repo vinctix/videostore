fa6c1bdbc95f0fcdb2e086ee0e92a72ab1e1af3e
vincent <vtixier1@me.com>
2023-02-26 17:18:04 -0500

    Remove long lines on the tests.

--- src/VideoStore.test.ts
+++ src/VideoStore.test.ts
@@ -12,7 +12,10 @@ describe("Videostore tests", () => {
   test("test Single New Release Statement", () => {
     customer.addRental(new Rental(new Movie("The Cell", Movie.NEW_RELEASE), 3));
     expect(customer.statement()).toBe(
-      "Rental Record for Fred\n\tThe Cell\t9.0\nYou owed 9.0\nYou earned 2 frequent renter points\n"
+      "Rental Record for Fred\n" +
+        "\tThe Cell\t9.0\n" +
+        "You owed 9.0\n" +
+        "You earned 2 frequent renter points\n"
     );
   });
 
@@ -22,7 +25,11 @@ describe("Videostore tests", () => {
       new Rental(new Movie("The Tigger Movie", Movie.NEW_RELEASE), 3)
     );
     expect(customer.statement()).toBe(
-      "Rental Record for Fred\n\tThe Cell\t9.0\n\tThe Tigger Movie\t9.0\nYou owed 18.0\nYou earned 4 frequent renter points\n"
+      "Rental Record for Fred\n" +
+        "\tThe Cell\t9.0\n" +
+        "\tThe Tigger Movie\t9.0\n" +
+        "You owed 18.0\n" +
+        "You earned 4 frequent renter points\n"
     );
   });
 
@@ -31,7 +38,10 @@ describe("Videostore tests", () => {
       new Rental(new Movie("The Tigger Movie", Movie.CHILDRENS), 3)
     );
     expect(customer.statement()).toBe(
-      "Rental Record for Fred\n\tThe Tigger Movie\t1.5\nYou owed 1.5\nYou earned 1 frequent renter points\n"
+      "Rental Record for Fred\n" +
+        "\tThe Tigger Movie\t1.5\n" +
+        "You owed 1.5\n" +
+        "You earned 1 frequent renter points\n"
     );
   });
 
@@ -43,7 +53,12 @@ describe("Videostore tests", () => {
     customer.addRental(new Rental(new Movie("Eraserhead", Movie.REGULAR), 3));
 
     expect(customer.statement()).toBe(
-      "Rental Record for Fred\n\tPlan 9 from Outer Space\t2.0\n\t8 1/2\t2.0\n\tEraserhead\t3.5\nYou owed 7.5\nYou earned 3 frequent renter points\n"
+      "Rental Record for Fred\n" +
+        "\tPlan 9 from Outer Space\t2.0\n" +
+        "\t8 1/2\t2.0\n" +
+        "\tEraserhead\t3.5\n" +
+        "You owed 7.5\n" +
+        "You earned 3 frequent renter points\n"
     );
   });
 });
